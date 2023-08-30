c5b9100bc1899e182ed16c70dda95f4d3e36b8ef
Mike Turner <mike.turner@anu.edu.au>
2023-08-30 19:40:50 +0800

    Adds ability to censor hits

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import { readFile, writeFile } from '../helpers/file-helpers';
-
-const DATABASE_PATH = '/src/database.json';
+import HitCounter from '../components/HitCounter';
+import Censored from '../components/Censored';
 
 function Home() {
-  let { hits } = JSON.parse(readFile(DATABASE_PATH));
-  hits += 1;
-  writeFile(DATABASE_PATH, JSON.stringify({ hits }));
-
   return (
     <main>
       <h1>Hello Next!</h1>
-      <p>You are visitor number {hits}.</p>
+      <p>
+        You are visitor number <Censored><HitCounter /></Censored>.
+      </p>
     </main>
   );
 }
